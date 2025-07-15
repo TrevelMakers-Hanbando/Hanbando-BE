7a64dadc7105534d277dd677f1cb86cf3cc2f7f0
이성훈 <dgh0612321@naver.com>
2025-07-15 21:45:20 +0900

    백엔드 1차 기본 세팅 완료

---
+++ src/main/java/com/springboot/hanbandobe/HanbandoBeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hanbandobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbandoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanbandoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/hanbandobe/HanbandoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hanbandobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanbandoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
