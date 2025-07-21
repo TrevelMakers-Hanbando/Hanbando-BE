3fba42eba968c479f826a316e40890b1556d8bf9
이성훈 <dgh0612321@naver.com>
2025-07-21 22:56:06 +0900

    선택이랑 선택취소 수정

--- src/main/java/com/springboot/hanbandobe/domain/schedule_detail/service/ScheduleDetailServiceImpl.java
+++ src/main/java/com/springboot/hanbandobe/domain/schedule_detail/service/ScheduleDetailServiceImpl.java
@@ -45,7 +45,7 @@ public class ScheduleDetailServiceImpl implements ScheduleDetailService {
         if (Boolean.TRUE.equals(scheduleDetail.getIsSelected())) throw new RuntimeException("이미 선택된 일정입니다.");
 
 
-        scheduleDetail.setIsSelected(Boolean.FALSE);
+        scheduleDetail.setIsSelected(Boolean.TRUE);
         scheduleDetailRepository.save(scheduleDetail);
 
         return ScheduleDetailResponseDto.builder()
@@ -67,7 +67,7 @@ public class ScheduleDetailServiceImpl implements ScheduleDetailService {
 
         if (Boolean.FALSE.equals(scheduleDetail.getIsSelected())) throw new RuntimeException("선택된 적이 없는 일정입니다.");
 
-        scheduleDetail.setIsSelected(Boolean.TRUE);
+        scheduleDetail.setIsSelected(Boolean.FALSE);
         scheduleDetailRepository.save(scheduleDetail);
 
         return ScheduleDetailResponseDto.builder()
