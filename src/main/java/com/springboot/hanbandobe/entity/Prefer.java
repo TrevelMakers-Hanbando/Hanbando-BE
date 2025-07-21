2233f8f322da7d83861b44f0abc09d15ca310c82
이성훈 <dgh0612321@naver.com>
2025-07-21 20:37:59 +0900

    prefer table 생성

--- src/main/java/com/springboot/hanbandobe/entity/Prefer.java
+++ src/main/java/com/springboot/hanbandobe/entity/Prefer.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
@@ -18,12 +19,12 @@ public class Prefer extends BaseEntity {
     @EmbeddedId
     private PreferId id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @MapsId("userNo")
     @JoinColumn(name = "user_no")
     private User user;
 
-    @Column(name = "where", nullable = false, columnDefinition = "TEXT")
+    @Column(name = "`where`", nullable = false, columnDefinition = "TEXT")
     private String where;
 
     @Column(name = "content", nullable = false, columnDefinition = "TEXT")
