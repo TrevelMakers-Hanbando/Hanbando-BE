76f4075ff0ec75916c6f62e97cbfd9d4779ac760
Hyeonjunnn <dev_sklg0602@naver.com>
2025-07-23 17:09:25 +0900

    [송현준] @EnableJpaAuditing 추가

--- src/main/java/com/springboot/hanbandobe/HanbandoBeApplication.java
+++ src/main/java/com/springboot/hanbandobe/HanbandoBeApplication.java
@@ -2,8 +2,10 @@ package com.springboot.hanbandobe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HanbandoBeApplication {
 
     public static void main(String[] args) {
