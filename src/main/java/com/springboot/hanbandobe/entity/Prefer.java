4503fae6fcbc2fb5f403aeeddcd4b7d4bd66c40c
이성훈 <dgh0612321@naver.com>
2025-07-28 15:54:30 +0900

    선호도 관련 crud

--- src/main/java/com/springboot/hanbandobe/entity/Prefer.java
+++ src/main/java/com/springboot/hanbandobe/entity/Prefer.java
@@ -5,21 +5,34 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "prefer")
 public class Prefer extends BaseEntity {
     @EmbeddedId
+    @Id
     private PreferId id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @MapsId("userNo")
     @JoinColumn(name = "user_no")
     private User user;
@@ -35,8 +48,25 @@ public class Prefer extends BaseEntity {
 
 
     @Embeddable
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
     public static class PreferId implements Serializable {
         @Column(name = "user_no")
         private Long userNo;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof PreferId)) return false;
+            PreferId that = (PreferId) o;
+            return Objects.equals(userNo, that.userNo);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(userNo);
+        }
     }
 }
