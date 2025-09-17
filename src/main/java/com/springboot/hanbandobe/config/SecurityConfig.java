a09669789b5e7ce2c3fe677285f661797aaa645d
kimdoyun0806 <gim_doyun@naver.com>
2025-09-17 15:38:36 +0900

    [김도윤] CORS 허용

--- src/main/java/com/springboot/hanbandobe/config/SecurityConfig.java
+++ src/main/java/com/springboot/hanbandobe/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
             CorsConfiguration configuration = new CorsConfiguration();
 
             // CORS 요청에서 허용할 출처를 설정
-            configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:3000"));
+            configuration.setAllowedOrigins(Arrays.asList("https://www.travel-hanbando.com", "http://localhost:8080", "http://localhost:3000"));
 
             // CORS 요청에서 허용할 HTTP 메소드 지정
             configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"));
